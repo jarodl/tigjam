04ff37edbf32ac6f38cc42bcc25de5f09e1fabfa
Jarod Luebbert <jarodluebbert@gmail.com>
2011-10-14 18:51:22 -0700

    Initial Commit

---
+++ tigjam/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  tigjam
+//
+//  Created by Jarod Luebbert on 10/14/11.
+//  Copyright MindSnacks 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ tigjam/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  tigjam
+//
+//  Created by Jarod Luebbert on 10/14/11.
+//  Copyright MindSnacks 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
